16b24e6525bf0b69b9f088dff6a904d773300000
Shikhar <shikhar.varshney2003@gmail.com>
2026-01-04 15:48:15 +0530

    Intialize frontend

---
+++ frontend/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Register New User
+    </div>
+  )
+}
+
+export default page
---
+++ frontend/app/(auth)/signIn/page.tsx
@@ -0,0 +1,12 @@
+
+"use client"
+
+const page = () => {
+  return (
+    <div>
+      sign In Page
+    </div>
+  )
+}
+
+export default page
