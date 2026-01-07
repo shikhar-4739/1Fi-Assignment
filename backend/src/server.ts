67ac4d4e2672d1787901afbe8f177514719842f1
Shikhar <shikhar.varshney2003@gmail.com>
2026-01-07 15:17:20 +0530

    Updates

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import app from './app.js'
 
-const PORT = process.env.PORT || 5000
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`)
+  })
+}
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`)
-})
+export default app
